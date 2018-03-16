6ffe284ed01a6a589d371e0b12692d4a174fdf15
Cora Quezada <corafenice1013@gmail.com>
2018-03-16 19:50:17 -0400

    created comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180316220035) do
+ActiveRecord::Schema.define(version: 20180316234239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "picture_id"
+    t.text "usertext"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "pictures", force: :cascade do |t|
     t.string "title"
     t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
