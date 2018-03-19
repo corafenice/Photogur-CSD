9bcfa8f3a2a0b54980280222f6670d5e25f564cd
Cora Quezada <corafenice1013@gmail.com>
2018-03-19 09:27:45 -0400

    fixed sign in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.
+            find_by(username: params[:username]).
+            try(:authenticate, params[:password])
+
+    return render action: "new" unless @user
+
+    #Logged in, Hooray
+    session[:user_id] = @user.id
+    redirect_to pictures_path
+  end
+end
